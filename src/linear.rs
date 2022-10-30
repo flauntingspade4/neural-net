154f5563e1efb7234e7044238cdbc5ed01bbb2cb
flauntingspade4 <elliotwhybrow@gmail.com>
2022-10-30 18:52:15 +0000

    Make `LinearLayer` generic over an `ActivationFunction`

--- src/linear.rs
+++ src/linear.rs
@@ -1,4 +1,7 @@
+use std::marker::PhantomData;
+
 use crate::{
+    activation::ActivationFunction,
     ghost::{GhostCell, GhostToken},
     matrix::Matrix,
     Model,
@@ -7,16 +10,25 @@ use crate::{
 #[cfg(feature = "random_generation")]
 use rand::rngs::ThreadRng;
 
-pub struct LinearLayer<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> {
+pub struct LinearLayer<
+    'id,
+    const INPUT_LEN: usize,
+    const OUTPUT_LEN: usize,
+    T: ActivationFunction<OUTPUT_LEN>,
+> {
     weights: GhostCell<'id, Matrix<OUTPUT_LEN, INPUT_LEN>>,
     biases: GhostCell<'id, Matrix<OUTPUT_LEN, 1>>,
+    activation: PhantomData<T>,
 }
 
-impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> LinearLayer<'id, INPUT_LEN, OUTPUT_LEN> {
+impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize, T: ActivationFunction<OUTPUT_LEN>>
+    LinearLayer<'id, INPUT_LEN, OUTPUT_LEN, T>
+{
     pub fn new(weights: Matrix<OUTPUT_LEN, INPUT_LEN>, biases: Matrix<OUTPUT_LEN, 1>) -> Self {
         Self {
             weights: GhostCell::new(weights),
             biases: GhostCell::new(biases),
+            activation: PhantomData,
         }
     }
 }
@@ -37,35 +49,41 @@ impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> Clone
     }
 }*/
 
-impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> Default
-    for LinearLayer<'id, INPUT_LEN, OUTPUT_LEN>
+impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize, T: ActivationFunction<OUTPUT_LEN>>
+    Default for LinearLayer<'id, INPUT_LEN, OUTPUT_LEN, T>
 {
     fn default() -> Self {
         Self {
             weights: GhostCell::new(Matrix::new_zeroed()),
             biases: GhostCell::new(Matrix::new_zeroed()),
+            activation: PhantomData,
         }
     }
 }
 
-impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> Model<'id, INPUT_LEN, OUTPUT_LEN>
-    for LinearLayer<'id, INPUT_LEN, OUTPUT_LEN>
+impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize, T: ActivationFunction<OUTPUT_LEN>>
+    Model<'id, INPUT_LEN, OUTPUT_LEN> for LinearLayer<'id, INPUT_LEN, OUTPUT_LEN, T>
 {
     fn forward(
         &self,
         token: &GhostToken<'id>,
         matrix: &Matrix<INPUT_LEN, 1>,
     ) -> Matrix<OUTPUT_LEN, 1> {
-        *self.weights.borrow(token) * *matrix + *self.biases.borrow(token)
+        let pre_activation = *self.weights.borrow(token) * *matrix + *self.biases.borrow(token);
+
+        T::activate(pre_activation)
     }
 }
 
 #[cfg(feature = "random_generation")]
-impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize> LinearLayer<'id, INPUT_LEN, OUTPUT_LEN> {
+impl<'id, const INPUT_LEN: usize, const OUTPUT_LEN: usize, T: ActivationFunction<OUTPUT_LEN>>
+    LinearLayer<'id, INPUT_LEN, OUTPUT_LEN, T>
+{
     pub fn random_new(rng: &mut ThreadRng) -> Self {
         Self {
             weights: GhostCell::new(Matrix::random_new(rng)),
             biases: GhostCell::new(Matrix::random_new(rng)),
+            activation: PhantomData,
         }
     }
 }
